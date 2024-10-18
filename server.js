4a6896aaf880df3029c975a43d96e18b3e803b2d
lismanurmalaa <lismanurmala76@gmail.com>
2024-10-18 18:54:04 +0700

    first commit: init project

---
+++ server.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const morgan = require("morgan");
+const { Students } = require("./models");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.post("/api/v1/students", async (req, res) => {
+    const newStudent = req.body
+    console.log(newStudent)
+
+    try {
+    await Students.create(newStudent);
+        res.status(200).json({
+            status: "Success",
+            message: "Application passed healtcheck",
+            isSuccess: true
+        })
+    } catch(error) {
+        res.status(500).json({
+            status: "Fail",
+            message: error.message,
+            isSuccess: false
+        })
+    }
+})
+
+app.get("/api/v1/students", async (req, res) => {
+    try {
+    const students = await Students.findAll();
+        res.status(200).json({
+            status: "Success",
+            message: "Application passed healtcheck",
+            isSuccess: true,
+            data: {
+                students
+            }
+        })
+    } catch(error) {
+        res.status(500).json({
+            status: "Fail",
+            message: error.message,
+            isSuccess: false
+        })
+    }
+})
+
+app.get("/api/v1/health-check", async (req, res) => {
+    try {
+        res.status(200).json({
+            status: "Success",
+            message: "Application passed healtcheck",
+            isSuccess: true
+        })
+    } catch(error) {
+        res.status(500).json({
+            status: "Fail",
+            message: "Application fail pass healtcheck",
+            isSuccess: false
+        })
+    }
+})
+
+module.exports = app;
